0289c37cdb1fcf3b83e85c06080a0880e34b3a63
miitchpls <mitchgrek@gmail.com>
2026-05-22 18:04:29 +0200

    .

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,10 @@
 """Shim that re-exports tests.common from pytest-homeassistant-custom-component."""
 
 from pytest_homeassistant_custom_component.common import *  # noqa: F401, F403
+from pytest_homeassistant_custom_component.common import (  # noqa: F401
+    MockConfigEntry,
+    async_fire_time_changed,
+    load_json_array_fixture,
+    load_json_object_fixture,
+    snapshot_platform,
+)
